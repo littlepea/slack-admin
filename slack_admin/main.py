d9ee7801f49c979f69a39e5163dcdccba37d8199
Evgeny Demchenko <little_pea@list.ru>
2017-03-14 18:38:04 +0800

    Refactored to use asyncio

--- slack_admin/main.py
+++ slack_admin/main.py
@@ -2,11 +2,10 @@ import asyncio
 import os
 import time
 
+import aiohttp
 import click
-import parsedatetime
 import humanfriendly
-
-from slackclient import SlackClient
+import parsedatetime
 
 
 @click.group()
@@ -31,69 +30,81 @@ def cleanup(older, bigger, prompt, token):
     older_than, parse_status = cal.parseDT(datetimeString=older)
     older_than_timestamp = int(time.mktime(older_than.timetuple()))
     if not parse_status:
-        click.echo('Can\'t parse the date: {}'.format(older))
+        print('Can\'t parse the date: {}'.format(older))
         return
 
     try:
-        num_bytes = humanfriendly.parse_size(bigger)
+        bigger_than = humanfriendly.parse_size(bigger)
     except humanfriendly.InvalidSize:
-        click.echo(f'Can\'t parse the file size: {bigger}')
+        print(f'Can\'t parse the file size: {bigger}')
         return
     except TypeError:
-        num_bytes = None
+        bigger_than = None
 
     token = token or os.environ.get('SLACK_API_TOKEN')
     if not token:
-        click.echo('Please, provide --token or set SLACK_API_TOKEN env variable')
+        print('Please, provide --token or set SLACK_API_TOKEN env variable')
         return
-
-    prompt_message = f'Going to delete all files older than {older}' + \
-                     (f' and bigger than {bigger}' if num_bytes else f'')
-
-    click.echo(prompt_message)
-    if prompt and input('Do you agree? [y/n]').lower() != 'y':
-        click.echo('Canceled deleting files')
-        return
-
-    client = SlackClient(token)
-    space_cleared = 0
-
-    first_page = client.api_call('files.list', ts_to=older_than_timestamp)
-    files = first_page['files']
 
     event_loop = asyncio.get_event_loop()
     try:
-        files += event_loop.run_until_complete(
-            collect_files(client, older_than_timestamp, start_page=2, total_pages=first_page['paging']['pages'])
+        files = event_loop.run_until_complete(
+            get_files(token, older_than_timestamp, bigger_than)
         )
 
-        for file in files:
-            space_cleared += event_loop.run_until_complete(
-                delete_file(client, file)
-            )
+        print(f'Going to delete all files older than {older}' +
+              (f' and bigger than {bigger}' if bigger_than else f''))
+        print(f'{len(files)} will be deleted')
+        if prompt and input('Do you agree? [y/n]').lower() != 'y':
+            print('Canceled deleting files')
+            return
+
+        space_cleared = event_loop.run_until_complete(
+            delete_files(token, files)
+        )
     finally:
         event_loop.close()
 
+    print(f'Deleted {len(files)} files, saved {space_cleared / 1000000}Mb')
+
+
+async def get_files(token, older_than_timestamp, bigger_than=None):
+    files = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://slack.com/api/files.list',
+                               params={'token': token, 'ts_to': older_than_timestamp}) as resp:
+            first_page = await resp.json()
+            files += first_page['files']
+
+        for page in range(2, first_page['paging']['pages'] + 1):
+            async with session.get('https://slack.com/api/files.list',
+                                   params={'token': token, 'ts_to': older_than_timestamp, 'page': page}) as resp:
+                current_page = await resp.json()
+                files += current_page['files']
+
+    if bigger_than:
+        return [file for file in files if file['size'] > bigger_than]
+
+    return files
+
+
+async def delete_files(token, files):
+    space = 0
+    count = 1
     num_files = len(files)
-    click.echo(f'Deleted {num_files} files, saved {space_cleared / 1000000}Mb')
+    async with aiohttp.ClientSession() as session:
+        for file in files:
+            async with session.get('https://slack.com/api/files.delete',
+                                   params={'token': token, 'file': file['id']}) as resp:
+                result = await resp.json()
 
-
-async def collect_files(client, older_than_timestamp, start_page=2, total_pages=2):
-    all_files = []
-    for page in range(start_page, total_pages+1):
-        files = client.api_call('files.list', ts_to=older_than_timestamp, page=page)
-        all_files += files['files']
-    return all_files
-
-
-async def delete_file(client, file):
-    result = client.api_call('files.delete', file=file['id'])
-    if result['ok']:
-        click.echo(f'deleted file "{file["name"]}"')
-        return file['size']
-    else:
-        click.echo(f'failed to delete file "{file["name"]}" because of "{result["error"]}"')
-        return 0
+                if result['ok']:
+                    print(f'deleted file {count} of {num_files}: "{file["name"]}"')
+                    space += file['size']
+                    count += 1
+                else:
+                    print(f'failed to delete file "{file["name"]}" because of "{result["error"]}"')
+    return space
 
 
 if __name__ == '__main__':
